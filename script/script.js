6a0e05c0256100b5f84300e7b8fe40bac3cbf9b8
Priya <priyannayak100@gmail.com>
2021-08-21 19:56:55 +0530

    added filter options

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
 let dropDownMenu=document.querySelector("#drop-down-menu-btn");
 let dropDownMenuItems=document.querySelector(".drop-down-menu-items");
+let filterBtn=document.querySelector("#filter");
+let filterOptions=document.querySelector(".filter-options");
 
 dropDownMenu.addEventListener("click",showMenu);
+filterBtn.addEventListener('click',showFilterOpt)
+
 function showMenu()
 {
    dropDownMenuItems.classList.toggle("all");
+}
+
+function showFilterOpt()
+{
+    filterOptions.classList.toggle("all");
 }
