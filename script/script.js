1067953e2fcdcd79f0bee0b1cb39003c1f047ae4
Priya <priyannayak100@gmail.com>
2021-08-22 16:40:22 +0530

    added post template

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
+let body=document.querySelector("body");
+let container=document.querySelector("#container");
+
+
+//dropdown menu 
 let dropDownMenu=document.querySelector("#drop-down-menu-btn");
 let dropDownMenuItems=document.querySelector(".drop-down-menu-items");
 let filterBtn=document.querySelector("#filter");
@@ -14,4 +19,22 @@ function showMenu()
 function showFilterOpt()
 {
     filterOptions.classList.toggle("all");
+}
+
+//post 
+let postBtn=document.querySelector("#post-btn");
+let postTemplate=document.querySelector(".post-content");
+let cancelPost=document.querySelector("#cancel");
+let successPost=document.querySelector("#posted-content");
+
+postBtn.addEventListener("click",addpost);
+cancelPost.addEventListener("click",addpost);
+successPost.addEventListener("click",addpost);
+
+function addpost()
+{
+    postTemplate.classList.toggle("all");
+    container.classList.toggle("opacity");
+    body.classList.toggle("overflow");
+    window.scrollTo(0,0)
 }
