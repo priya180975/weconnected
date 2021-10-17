d1cbb660e04d29ab280208090e754c19e8008cf8
Priya <priyannayak100@gmail.com>
2021-10-17 15:41:39 +0530

    separated content in files

---
+++ main/main.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="icon" type="image/svg" href="../images/logo-dots.svg">
+    <link rel="stylesheet" type="text/css" href="../stylesheet/main.css">
+    <title>WeConnected | Main Page</title>
+</head>
+<body>
+
+    <div id="container">
+       
+        <!--nav bar-->
+        <?php include_once 'navbar.php'; ?>
+         
+        <main>
+            <div id="right-desk">
+                <!--profile-->
+                <?php include_once 'profiledisplay.php'?>
+
+                <!--filter-->
+                <?php include_once 'filter.php'?>
+            </div>
+
+            <!--main content-->
+            <div id="content-all">
+                
+                <!-- All posts -->
+                <?php include_once 'allposts.php' ?>
+
+            </div>
+
+            <!--committee-->
+            <?php include_once "committee.php";?>
+
+        </main>
+    </div>
+
+    <!--back to top-->
+    <div id="back-to-top-div"><button id="back-to-top" class="btn">up</button></div>
+
+    <!--Post template-->
+    <?php include_once 'posttemplate.php' ?>
+
+    <script src="../javascript/main.js"></script>
+    
+</body>
+</html>
